61e5bb8196df92b9c051ec0a4a91e52cb45d8ef2
cyrielle <lacyrielle@gmail.com>
2020-04-13 23:46:15 +0200

    first function of basics

---
+++ lib/03_basics.rb
@@ -0,0 +1,15 @@
+def who_is_bigger(a, b, c)
+	if a==nil or b==nil or c==nil
+		return "nil detected"
+		
+	end
+	if a>=b and a>=c
+		return "a is bigger"
+	elsif b>=c
+		return "b is bigger"
+	else
+		return "c is bigger"
+	end
+			
+		
+end
